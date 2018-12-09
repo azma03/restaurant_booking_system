1d9f67f4fd4885721bfeab7e34503249b9162b0e
Asma <asma.virgo85@gmail.com>
2018-12-09 18:36:20 +0000

    corrects the booking custom repo name

--- backend/restaurant/src/main/java/com/codeclan/example/restaurant/repositories/bookings/BookingRepositoryImpl.java
+++ backend/restaurant/src/main/java/com/codeclan/example/restaurant/repositories/bookings/BookingRepositoryImpl.java
@@ -2,5 +2,5 @@ package com.codeclan.example.restaurant.repositories.bookings;
 
 import com.codeclan.example.restaurant.repositories.booths.BoothRepositoryCustom;
 
-public class BookingRepositoryImpl implements BoothRepositoryCustom {
+public class BookingRepositoryImpl implements BookingRepositoryCustom {
 }
