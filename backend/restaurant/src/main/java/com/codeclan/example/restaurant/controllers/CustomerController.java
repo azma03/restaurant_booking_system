75b775fe55d73fac54e8292b221c88d9e3668c3c
Asma <asma.virgo85@gmail.com>
2018-12-09 15:18:02 +0000

    adds basic criteria query but issues still need a fix

--- backend/restaurant/src/main/java/com/codeclan/example/restaurant/controllers/CustomerController.java
+++ backend/restaurant/src/main/java/com/codeclan/example/restaurant/controllers/CustomerController.java
@@ -1,9 +1,23 @@
 package com.codeclan.example.restaurant.controllers;
 
+import com.codeclan.example.restaurant.models.Customer;
+import com.codeclan.example.restaurant.repositories.customers.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/customers")
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/sortBy/frequencyVisit")
+    public List<Customer> getCustomersByVisitFrequency(){
+        return customerRepository.getCustomersByVisitFrequency();
+    }
 }
--- backend/restaurant/src/main/java/com/codeclan/example/restaurant/repositories/customers/CustomerRepositoryImpl.java
+++ backend/restaurant/src/main/java/com/codeclan/example/restaurant/repositories/customers/CustomerRepositoryImpl.java
@@ -1,4 +1,75 @@
 package com.codeclan.example.restaurant.repositories.customers;
 
+import com.codeclan.example.restaurant.models.Customer;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.OrderBy;
+import java.util.List;
+
 public class CustomerRepositoryImpl implements CustomerRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Customer> getCustomersByVisitFrequency() {
+        List<Customer> results = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try{
+            Criteria cr = session.createCriteria(Customer.class, "c");
+//            cr.createAlias("c.bookings", "bookings").addOrder(Order.desc("bookings.id"));
+            cr.createAlias("c.bookings", "bookings");
+//            cr.setProjection(Projections.projectionList().add(Projections.count("bookings.id"), "countBookings"));
+//            cr.addOrder(Order.desc("countBookings"));
+
+
+//            cr.createAlias("c.bookings", "bookings");   //s: customer.bookings, s1: Alias for the Booking class
+//            cr.createCriteria("bookings").addOrder(Order.desc("bookings.id"));
+//            cr.setProjection(Projections.count("bookings.id").as("BookingCount"));
+            cr.setProjection(Projections.projectionList().add(Projections.groupProperty("bookings.Id"))
+                    .add(Projections.count("bookings.id").as("BookingCount")));
+            cr.addOrder(Order.desc("BookingCount"));
+
+
+
+//            cr.addOrder(Order.desc("bookings"));
+//            cr.createCriteria("bookings").add(Restrictions.eq("partySize", 2));
+//            cr.createAlias("customers", "customer");
+//            cr.add(Restrictions.eq("name", "Asma"));
+            results = (List<Customer>) cr.list();
+
+//            hibernateSession.createCriteria(UserGroup.class).addOrder( SizeOrder.asc("members") ).list();
+//
+//            session.createCriteria(News.class, "n");
+//            criteria.createAlias("n.comments", "comments");
+//            criteria.setProjection(Projections.projectionList()
+//                    .add(Projections.groupProperty("comments.id"))
+//                    .add(Projections.count("comments.id").as("numberOfComments")));
+//            criteria.addOrder(Order.desc("numberOfComments"));
+//            List<News> news = criteria.list();
+
+
+        } catch(HibernateException ex){
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+
+        return results;
+    }
 }
+
+
+// https://www.boraji.com/hibernate-5-criteria-query-example
+//https://docs.jboss.org/hibernate/orm/3.3/reference/en/html/querycriteria.html
