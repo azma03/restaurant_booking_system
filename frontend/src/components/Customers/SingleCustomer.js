ab0f613aeb19a56d0008eb11fae37f747d96b483
Stephen Hart <stephen@cardswitcher.co.uk>
2018-12-11 18:16:50 +0000

    customer totals not working

--- frontend/src/components/Customers/SingleCustomer.js
+++ frontend/src/components/Customers/SingleCustomer.js
@@ -1,5 +1,6 @@
 import React, {Component}  from 'react';
 import {Link} from 'react-router-dom';
+import Request from '../../helpers/request.js';
 
 class SingleCustomer extends Component {
   constructor(props){
@@ -12,50 +13,46 @@ class SingleCustomer extends Component {
     }
 
     this.onDelete = this.onDelete.bind(this);
-    this.totalSpendings = this.totalSpendings.bind(this);
-    this.totalVisits = this.totalVisits.bind(this);
-    this.discountLevel = this.discountLevel.bind(this);
+    // this.totalSpendings = this.totalSpendings.bind(this);
+    // this.totalVisits = this.totalVisits.bind(this);
+    // this.discountLevel = this.discountLevel.bind(this);
 
   }
 
-  componentDidMount(){
-
-  }
+  // componentDidMount(){
+  //   this.totalSpendings();
+  //   this.totalVisits();
+  //   this.discountLevel();
+  // }
 
  onDelete(){
    this.props.handleDelete(this.props.customer.id)
  }
 
- totalSpendings(){
-   // let totalSpend;
-    let request1 = new Request()
-    request1.get('api/customers/'+ this.props.customer.id + '/totalSpendings').then((data) => {
-      this.setState({totalSpend: data})
-      // return totalSpend;
-    })
-  }
-
-  totalVisits(){
-    // let visitCount;
-     let request2 = new Request()
-     request2.get('api/customers/'+ this.props.customer.id + '/visitCount').then((data) => {
-       this.setState({visitCount: data})
-       // return visitCount;
-     })
-   }
-
-  discountLevel(){
-     // let visits = totalVisits();
-     let visits = this.state.visitCount;
-     if(visits>10){
-       this.setState({discountLevel: "gold"})
-       // discountLevel = "gold"
-     }else if(visits>5){
-       this.setState({discountLevel: "silver"})
-     }else{
-      this.setState({discountLevel: "bronze"})
-     }
-    }
+ // totalSpendings(){
+ //    let request1 = new Request()
+ //    request1.get('api/customers/1/totalSpendings').then((data) => {
+ //      this.setState({totalSpend: data})
+ //    })
+ //  }
+ //
+ //  totalVisits(){
+ //     let request2 = new Request()
+ //     request2.get('api/customers/1/visitCount').then((data) => {
+ //       this.setState({visitCount: data})
+ //     })
+ //   }
+ //
+ //  discountLevel(){
+ //     let visits = this.state.visitCount;
+ //     if(visits>10){
+ //       this.setState({discountLevel: "gold"})
+ //     }else if(visits>5){
+ //       this.setState({discountLevel: "silver"})
+ //     }else{
+ //      this.setState({discountLevel: "bronze"})
+ //     }
+ //    }
 
 render(){
 
@@ -73,9 +70,9 @@ render(){
       <p>Name: {this.props.customer.name}</p>
       <p>Phone: {this.props.customer.phone}</p>
       <p>Email: {this.props.customer.email}</p>
-      <p>Discount: {this.state.discountLevel}</p>
-      <p>Total Visit: {this.state.totalVisits}</p>
-      <p>Total Spend: {this.state.totalSpendings}</p>
+      <p>Discount: {this.props.discountLevel}</p>
+      <p>Total Visit: {this.props.totalVisits}</p>
+      <p>Total Spend: {this.props.totalSpendings}</p>
       Bookings:
       <ul>
       {bookings}
